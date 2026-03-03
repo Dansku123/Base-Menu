ead87e4fe77476a4f9a73f7acdb783fd0a9229ef
Daniel <daniel.somervalli@gmail.com>
2026-03-03 18:12:59 +0200

    Second official batch of THE files

--- Assets/Scripts/FadeControl.cs
+++ Assets/Scripts/FadeControl.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections;
 public class FadeControl : MonoBehaviour
 {
 
@@ -9,20 +9,48 @@ public class FadeControl : MonoBehaviour
     private Image fadeObject;
     private Color originalColor;
 
+    private Coroutine coroutine; 
+
     void Awake()
     {
         fadeObject = GetComponent<Image>();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        fadeObject = GetComponent<Image>();
+        if (fadeObject == null)
+        {
+            Debug.LogError("FadeControl requires an Image component on the same GameObject.");
+            return;
+        }
         originalColor = fadeObject.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartFade (float targetAlpha)
     {
-        
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        coroutine = StartCoroutine(FadeRoutine(targetAlpha));
+    }
+    
+
+    private IEnumerator FadeRoutine(float targetAlpha)
+    {
+        if (coroutine != null)
+        {
+            float time = 0f;
+            float startAlpha = fadeObject.color.a;
+
+            while (time < fadeDuration)
+            {
+                time += Time.deltaTime;
+                Color colorChanged = fadeObject.color;
+                colorChanged.a = Mathf.Lerp(startAlpha, targetAlpha, time / fadeDuration);
+                fadeObject.color = colorChanged;
+                yield return null;
+            } 
+
+            Color finalColor = fadeObject.color;
+            finalColor.a = targetAlpha;
+            fadeObject.color = finalColor;
+        }
     }
 }
