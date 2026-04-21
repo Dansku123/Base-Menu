93526cd91d0034c8e224a6515f8daf2023267270
Daniel <daniel.somervalli@gmail.com>
2026-04-21 18:12:52 +0300

    Bounce

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,11 +2,32 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-
     public float startSpeed = 5f;
-    void Start()
+    public Rigidbody2D rb;
+
+    void Awake()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    void Start()
+    {
+        LaunchBall();
+    }
+
+    void LaunchBall()
+    {
+        rb.linearVelocity = Vector2.zero;
+
+        Vector2 startDirection = new Vector2(Random.Range(-1f, 1f), 1);
+        rb.linearVelocity = startDirection * startSpeed;
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.y < -5.0f)
+        {
+            LaunchBall();
+        }
+    }
 }
